11806e1419cd24e05c9565571b755682cc205f19
hilal.bursali <hilal.bursali@shazam.com>
2018-01-06 16:03:06 +0300

    added Search Types

--- elastic4s-core/src/main/scala/com/sksamuel/elastic4s/searches/SearchType.scala
+++ elastic4s-core/src/main/scala/com/sksamuel/elastic4s/searches/SearchType.scala
@@ -5,8 +5,7 @@ object SearchType {
 
   case object DfsQueryThenFetch extends SearchType
   case object QueryThenFetch extends SearchType
-
-  val DEFAULT = DfsQueryThenFetch
+  
   val QUERY_THEN_FETCH = QueryThenFetch
   val DFS_QUERY_THEN_FETCH = DfsQueryThenFetch
 }
